7104e5ed40d93a516619fab65de9c896b9258eda
acatarinaoaraujo <acoa_@hotmail.com>
2025-01-20 21:13:23 -1000

    add promises with async/await

--- my-app/src/app/api/contact/route.js
+++ my-app/src/app/api/contact/route.js
@@ -12,17 +12,40 @@ export async function POST(request) {
                 user: process.env.NEXT_PUBLIC_GMAIL_USER,
                 pass: process.env.GMAIL_PASS,
             },
-            debug: false,
-            logger: false,
+            secure: true,
         });
 
-        const info = await transporter.sendMail({
+        await new Promise((resolve, reject) => {
+            transporter.verify((error, success) => {
+                if (error) {
+                    console.error('Verification error:', error);
+                    reject(error);
+                } else {
+                    console.log('Server is ready to send messages');
+                    resolve(success);
+                }
+            });
+        });
+
+        const mailData = {
             from: `"${name}" <${email}>`,
             to: process.env.NEXT_PUBLIC_GMAIL_USER,
             subject: title,
             text: message,
-        });
+            html: `<p>${message}</p>`,
+        };
 
+        await new Promise((resolve, reject) => {
+            transporter.sendMail(mailData, (err, info) => {
+                if (err) {
+                    console.error('Error sending email:', err);
+                    reject(err);
+                } else {
+                    console.log('Email sent:', info);
+                    resolve(info);
+                }
+            });
+        });
 
         return new Response(JSON.stringify({ message: 'Email sent successfully!' }), { status: 200 });
     } catch (error) {
